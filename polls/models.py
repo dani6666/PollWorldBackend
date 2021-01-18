8c3bbcdc12423f046ff93f18449214403acb0474
Daniel324225 <dan324225@gmail.com>
2021-01-19 00:26:52 +0100

    changes recommended by IDENT

--- polls/models.py
+++ polls/models.py
@@ -16,14 +16,14 @@ class Poll(models.Model):
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
     short_description = models.TextField(max_length=50)
     description = models.TextField(max_length=500)
-    time_needed = models.TimeField()
+    time_needed = models.DurationField()
     rating = models.FloatField()
     assigned_users = models.ManyToManyField(CustomUser, related_name='assigned_polls', through='PollAssignment')
 
 class PollAssignment(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
-    assigned_date = models.DateTimeField()
+    assigned_date = models.DateTimeField(auto_now_add=True)
     completed_date = models.DateTimeField(null=True)
 
 class QuestionOption(models.Model):
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,9 @@
 from django.views import View
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 import random
-import datetime
+from datetime import timedelta
 
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
@@ -19,8 +20,7 @@ def create_polls(user: CustomUser):
         orgs = Company.objects.all()
         count = orgs.count()
         while count < 5:
-            org = Company(name="Firma nr " + str(count))
-            org.save()
+            Company.objects.create(name="Firma nr " + str(count))
             count += 1
         orgs = Company.objects.all()
         return orgs[0:5]
@@ -29,8 +29,7 @@ def create_polls(user: CustomUser):
         categories = Category.objects.all()
         count = categories.count()
         while count < 5:
-            cat = Category(name="Kategoria nr " + str(count))
-            cat.save()
+            Category.objects.create(name="Kategoria nr " + str(count))
             count += 1
         categories = Category.objects.all()
         return categories[0:5]
@@ -38,32 +37,25 @@ def create_polls(user: CustomUser):
     orgs = get_organizations()
     categories = get_categories()
     for i in range(5):
-        poll = Poll(name="Ankieta nr: " + str(i), company=orgs[i], price=random.randint(10, 50) * 10, category=categories[i], short_description="Krótki opis", description="Długi opis ankiety", time_needed=datetime.time(0, random.randint(5, 15), 0), rating=random.uniform(1, 5))
-        poll.save()
+        poll = Poll.objects.create(name="Ankieta nr: " + str(i), company=orgs[i], price=random.randint(10, 50) * 10, category=categories[i], short_description="Krótki opis", description="Długi opis ankiety", time_needed=timedelta(minutes=random.randint(5, 15)), rating=random.uniform(1, 5))
 
-        option = QuestionOption(option="Opcja 1")
-        option.save()
+        option = QuestionOption.objects.create(option="Opcja 1")
 
-        option2 = QuestionOption(option="Opcja 2")
-        option2.save()
+        option2 = QuestionOption.objects.create(option="Opcja 2")
 
-        question = Question(poll=poll, type=0, text="Pytanie jednokrotnego wyboru")
-        question.save()
+        question = Question.objects.create(poll=poll, type=0, text="Pytanie jednokrotnego wyboru")
 
         question.options.add(option)
         question.options.add(option2)
 
-        question = Question(poll=poll, type=1, text="Pytanie wielokrotnego wyboru")
-        question.save()
+        question = Question.objects.create(poll=poll, type=1, text="Pytanie wielokrotnego wyboru")
 
         question.options.add(option)
         question.options.add(option2)
 
-        question = Question(poll=poll, type=2, text="Pytanie otwarte")
-        question.save()
+        question = Question.objects.create(poll=poll, type=2, text="Pytanie otwarte")
 
-        assignment = PollAssignment(user=user, poll=poll, assigned_date=datetime.datetime.now(), completed_date=None)
-        assignment.save()
+        assignment = PollAssignment.objects.create(user=user, poll=poll, completed_date=None)
 
 
 class GetUserPolls(generics.RetrieveAPIView):
@@ -75,7 +67,7 @@ class GetUserPolls(generics.RetrieveAPIView):
         polls = user.assigned_polls.filter(pollassignment__completed_date__isnull=True)
         if polls.count() == 0: #TODO return "no polls available for user"
             create_polls(user)
-            polls = user.assigned_polls.all()
+            polls = user.assigned_polls.filter(pollassignment__completed_date__isnull=True)
 
         response = []
 
@@ -99,10 +91,7 @@ class GetPoll(generics.RetrieveAPIView):
     permission_classes = (IsAuthenticated,)
 
     def get(self, request, poll_id):
-        try:
-            poll = Poll.objects.get(pk=poll_id)
-        except:
-            return HttpResponse("No poll with specified id") #TODO change
+        poll = get_object_or_404(Poll, pk=poll_id)
 
         if not self.request.user in poll.assigned_users.all():
             return HttpResponse("Poll is not available for current user") #TODO change
