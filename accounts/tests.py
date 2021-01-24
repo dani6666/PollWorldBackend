965313d719b6fc9a6aec92450520d5d36e01b605
Daniel <danielwz99@gmail.com>
2021-01-24 01:41:16 +0100

    testing all router methods

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,8 +1,8 @@
+from unittest import mock
+
 from django.test import TestCase
 
-# Create your tests here.
-from rest_framework.test import APIRequestFactory
-
+from accounts.serializers import UserSerializer
 from accounts.views import UserViewSet
 
 class UserViewSetTests(TestCase):
@@ -11,5 +11,24 @@ class UserViewSetTests(TestCase):
         request = type('',(),{'data':{'email':'test@email.com', 'password':'pass123'}})()
 
         response = sut.register(request)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_get(self):
+        sut = UserViewSet()
+        request = type('',(),{'user':{'email':'test@email.com', 'password':'pass123'}})()
+
+        response = sut.get(request)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_personal_update(self):
+        sut = UserViewSet()
+        sut.serializer_class = mock.Mock(spec=UserSerializer)
+
+        request = type('',(),{'user':{'email':'test@email.com', 'password':'pass123'},
+                              'data':{'age':'23', 'sex':'false'}})()
+
+        response = sut.personal_update(request)
 
         self.assertEqual(response.status_code, 200)
